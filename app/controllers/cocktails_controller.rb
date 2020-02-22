f0a5c12e3e31ae0175195f2fafd7ec8f3f3ad6a6
Catarina Pimentel <acatarinapimentel@gmail.com>
2020-02-22 13:47:14 +0000

    exercise 2

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,34 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(user_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:cocktail).permit(:name)
+  end
+end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+  before_action :fetch_cocktail, only: [:create, :new]
+
+  def new
+    @dose = Dose.new
+    @dose.cocktail = @cocktail
+  end
+
+  def create
+    @dose = Dose.new(doses_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def fetch_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+end
